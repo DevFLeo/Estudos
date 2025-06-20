7bab7393187b714ac3e54a2e9cf1f418040254e6
Leonardo Vinicius de Oliveira Frez <leonardoviniciusfrez@gmail.com>
2025-06-20 19:40:26 -0400

    git add. git commit -m "Atualizando arquivos do Ted-Loja e Loja.Console" git push origin main git push origin main --force

--- Ted-Loja/Loja.Console/obj/Debug/net9.0/Loja.Console.AssemblyInfo.cs
+++ Ted-Loja/Loja.Console/obj/Debug/net9.0/Loja.Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e99b7449a08cfb3f31af87504a75b40c66c06f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c085396e23c793d9967bccfde2313e8a4704d2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
